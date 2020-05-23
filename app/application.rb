d46e2543a21ea78e6320f7c28ecbcc38f3d2241d
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-23 15:59:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,8 +23,14 @@ class Application
       end
     end
      elsif req.path.match(/add/)
-     search_term = req.params["q"]
-     resp.write 
+     search_term = req.params["item"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
+
     else
       resp.write "Path Not Found"
     end
