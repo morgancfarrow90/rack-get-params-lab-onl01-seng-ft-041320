582e87904026abed59e376c7a53d8f419568b61b
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-23 15:47:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,9 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |cart_item|
+        resp.write "#{cart_item}\n"
     else
       resp.write "Path Not Found"
     end
