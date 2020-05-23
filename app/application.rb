b6fad4d3248baf6dc287857fc2c3de9ec1442f1d
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-23 15:52:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
        if @@cart.empty?
-         
+       resp.write "Your cart is empty"
        elsif 
        @@cart.each do |cart_item|
         resp.write "#{cart_item}\n"
